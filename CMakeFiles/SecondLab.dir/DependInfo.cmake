573a634d66f780ef525200d504418472c11f553a
Simon Sirak <ssirak@kth.se>
2019-05-09 18:15:43 +0200

    Something shows up but it is inverted and very strong light. I believe it is due to the incorrect definition of the original end light position. We should probably have another vector with lights, and probably make a function that samples a position and direction (for spheres, if we assume it is like a sun, any direction on the surface should reflect similar amount of light, because hot glowing lava knows no angles). Right now, we start the light path from inside the light sphere which is not correct

--- CMakeFiles/SecondLab.dir/DependInfo.cmake
+++ CMakeFiles/SecondLab.dir/DependInfo.cmake
@@ -4,13 +4,13 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/repo/simonsirak/BDPT/skeleton.cpp" "/repo/simonsirak/BDPT/CMakeFiles/SecondLab.dir/skeleton.cpp.o"
+  "/afs/kth.se/home/s/s/ssirak/repo/BDPT/skeleton.cpp" "/afs/kth.se/home/s/s/ssirak/repo/BDPT/CMakeFiles/SecondLab.dir/skeleton.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include/SDL"
+  "/usr/include/SDL"
   "glm"
   )
 
--- skeleton.cpp
+++ skeleton.cpp
@@ -29,8 +29,8 @@ struct Intersection{
 };
 
 struct BDPTpath{
-	vec3 startP;
-	vec3 endP;
+	Intersection startP;
+	Intersection endP;
 	vector<Intersection> intersections;
 };
 
@@ -70,7 +70,7 @@ mat3 P; // Pitch rotation matrix (around x axis)
 /* Model */
 vector<Obj*> triangles;
 
-int numSamples = 500;
+int numSamples = 700;
 
 /* Light source */
 vec3 lightPos( 0, -0.5, -0.7 );
@@ -90,7 +90,7 @@ bool ClosestIntersection(
 );
 
 void FindPath(vec3 start, vec3 direction, int maxDepth, vector<Intersection>& intersections);
-void FindPathHelper(vec3 point, vec3 normalToPoint, vec3 directionToPoint, int currentDepth, int maxDepth, vector<Intersection>& intersections);
+void FindPathHelper(Intersection& i, vec3 directionToPoint, int currentDepth, int maxDepth, vector<Intersection>& intersections);
 
 vec3 calcRadianceToPoint(const BDPTpath& path, unsigned int i);
 
@@ -210,25 +210,44 @@ void Draw()
 
                 vec3 color( 0, 0, 0 );
 
-                buffer[x][y] += (TracePath(cameraPos, dir, 0) / float(numSamples));
-
-				/*vec3 lightDir = glm::normalize(R*vec3(0.5, 0.5, 0.5));
+				std::uniform_real_distribution<float> dis(0, 1.0);
+				float theta0 = 2*float(PI)*dis(rd);
+				float theta1 = acos(1 - 2*dis(rd));
+				vec3 lightDir = glm::normalize(vec3(sin(theta1)*sin(theta0), sin(theta1)*cos(theta0), cos(theta1)));
+				
 				BDPTpath path;
-				path.startP = cameraPos;
-				path.endP = lightPos;
+				path.startP = {cameraPos, vec3(0,0,0), -1, -1}; // normal not needed
+				path.endP = {lightPos, vec3(0, 0, 0), -1, triangles.size()-1}; // normal not needed
 				vector<Intersection> cameraIntersections;
-				FindPath(path.startP, dir, 4, cameraIntersections);
+				FindPath(path.startP.position, dir, 4, cameraIntersections);
 				vector<Intersection> lightIntersections;
-				FindPath(path.endP, lightDir, 4, lightIntersections);
+				FindPath(path.endP.position, lightDir, 4, lightIntersections);
+
+				if(cameraIntersections.size() == 0 || lightIntersections.size() == 0){ // nothing was hit
+					continue;
+				}
+
+				Intersection& lastEye = cameraIntersections[cameraIntersections.size()-1];
+				Intersection& lastLight = lightIntersections[lightIntersections.size()-1];
+
+				Intersection i;
+				ClosestIntersection(lastEye.position, lastLight.position - lastEye.position, triangles, i);
+				if(i.t < glm::length(lastLight.position-lastEye.position)){ // something is blocking, don't add connection
+					continue;
+				}
+
 				for(int i = 0 ; i < cameraIntersections.size() ; ++i){
 					path.intersections.push_back(cameraIntersections[i]);
-					cout << i << endl;
+					//cout << i << endl;
 				}
 				for(int i = lightIntersections.size() - 1 ; i >= 0 ; --i){
 					path.intersections.push_back(lightIntersections[i]);
 				}
-				color += calcRadianceToPoint(path, 0);
-				color /= numSamples;*/
+
+				//color += calcRadianceToPoint(path, 0);
+				//color /= numSamples;
+
+				buffer[x][y] += (calcRadianceToPoint(path, 0) / float(numSamples));
 
                 PutPixelSDL( screen, x, y,  buffer[x][y]);
             }
@@ -268,68 +287,22 @@ bool ClosestIntersection(
 	}
 }
 
-vec3 DirectLight( const Intersection& i ){
-	
-	// get radius from sphere defined by light position 
-	// and the point of intersection.
-	vec3 radius = (lightPos - i.position);
-	float r2 = glm::length(radius)*glm::length(radius); // r^2
-	radius = glm::normalize(radius); // normalize for future calculations
+vec3 TracePath(vec3 start, vec3 dir, int depth) {
 
-	vec3 light = lightColor/float(4.0f*PI*r2); // calculate irradiance of light
-
-	/* 
-		calculate normal in direction based on source of ray
-		for the "first ray", it is the camera position that 
-		is the source. However, for multiple bounces you would 
-		need to embed the ray source into the Intersection 
-		struct so that the correct normal can be calculated
-		even when the camera is not the source.
-		This makes the model only take the viewable surface into
-		consideration. Note however that for any given ray source, only 
-		one side of the surface is ever viewable , so this all works 
-		out in the end. Multiple bounces would have different sources,
-		so it is still possible that the other side of a surface can 
-		receive light rays. Just not from light hitting the other side 
-		of the surface.
-	*/
-
-	vec3 sourceToLight = cameraPos - i.position;
-	vec3 normal = glm::dot(sourceToLight, triangles[i.triangleIndex]->normal(i.position)) * triangles[i.triangleIndex]->normal(i.position) / glm::dot(triangles[i.triangleIndex]->normal(i.position), triangles[i.triangleIndex]->normal(i.position));
-	normal = glm::normalize(normal);
-
-	/*
-		Direction needs to either always or never be normalised. 
-		Because it is not normalised in the draw function, I 
-		will not normalize here.
-		Also, I use a shadow bias (tiny offset in normal direction)
-		to avoid "shadow acne" which is caused by self-intersection.
-	*/
-
-	Intersection blocker;
-	if(ClosestIntersection(i.position + normal * 0.0001f, (lightPos - i.position), triangles, blocker) && glm::length(blocker.position - i.position) <= glm::length(lightPos-i.position)){
-		return vec3(0, 0, 0);
-	} else {
-		return triangles[i.triangleIndex]->color * light * (glm::dot(radius, normal) > 0.0f ? glm::dot(radius, normal) : 0.0f);
+	if (depth >= 5) {
+		return vec3(0, 0, 0) ;  // Bounced enough times
 	}
-}
-
-  vec3 TracePath(vec3 start, vec3 dir, int depth) {
-
-    if (depth >= 5) {
-      	return vec3(0, 0, 0) ;  // Bounced enough times
-    }
 
 	Intersection i;
-    if (!ClosestIntersection(start, dir, triangles, i)) {
-      	return vec3(0, 0, 0);  // Nothing was hit.
-    }
+	if (!ClosestIntersection(start, dir, triangles, i)) {
+		return vec3(0, 0, 0);  // Nothing was hit.
+	}
 
-    // Pick a random direction from here and keep going.
+	// Pick a random direction from here and keep going.
 
-    vec3 newstart = i.position;
+	vec3 newstart = i.position;
 
-    // This is NOT a cosine-weighted distribution!
+	// This is NOT a cosine-weighted distribution!
 
 	// RNG vector in hemisphere
 	std::uniform_real_distribution<float> dis(0, 1.0);
@@ -338,48 +311,48 @@ vec3 DirectLight( const Intersection& i ){
 	float theta1 = acos(1 - 2*dis(rd));
 
 	vec3 newdir = vec3(sin(theta1)*sin(theta0), sin(theta1)*cos(theta0), cos(theta1)); // http://corysimon.github.io/articles/uniformdistn-on-sphere/ THIS WAS A BIG ISSUE, WE FOLLOWED THE STACK OVERFLOW PIECES OF SHIEET. This fixed the "cross light on the ceiling"
-    vec3 surfaceToLight = glm::normalize(start - newstart);
+	vec3 surfaceToLight = glm::normalize(start - newstart);
 
 	newdir = glm::normalize(newdir);
 	// enforce direction to be in correct hemisphere, aka project normal onto random vector and normalize
-    vec3 normal = glm::dot(surfaceToLight, triangles[i.triangleIndex]->normal(newstart)) * triangles[i.triangleIndex]->normal(newstart) / glm::dot(triangles[i.triangleIndex]->normal(newstart), triangles[i.triangleIndex]->normal(newstart));
+	vec3 normal = glm::dot(surfaceToLight, triangles[i.triangleIndex]->normal(newstart)) * triangles[i.triangleIndex]->normal(newstart) / glm::dot(triangles[i.triangleIndex]->normal(newstart), triangles[i.triangleIndex]->normal(newstart));
 	normal = glm::normalize(normal); // HAS to be normalized after projection onto another vector.
 	newdir = glm::dot(newdir, normal) * newdir / glm::dot(newdir, newdir);
 	newdir = glm::normalize(newdir);
 
 
-    // Compute the BRDF for this ray (assuming Lambertian reflection)
+	// Compute the BRDF for this ray (assuming Lambertian reflection)
 
-    float cos_theta = glm::dot(newdir, normal); // note that the cosine factor is part of the Rendering Equation integrand/differential, NOT the BRDF.
-    vec3 BRDF = triangles[i.triangleIndex]->color / float(PI); // just the reflectance of the material / PI for Lambertian Reflection: http://www.oceanopticsbook.info/view/surfaces/lambertian_brdfs
+	float cos_theta = glm::dot(newdir, normal); // note that the cosine factor is part of the Rendering Equation integrand/differential, NOT the BRDF.
+	vec3 BRDF = triangles[i.triangleIndex]->color / float(PI); // just the reflectance of the material / PI for Lambertian Reflection: http://www.oceanopticsbook.info/view/surfaces/lambertian_brdfs
 
-    // Probability distribution function for lambertian reflection BRDF
-    // const vec3 p = glm::abs(glm::dot(normal, -dir)) * BRDF; // dot between normal and origin ray (ONLY FOR EYE PATH) (THIS DOES NOT WORK WITH LAMBERTIAN REFLECTION SINCE THE BRDF EVALUATIONS WILL CANCEL EACH OTHER OUT AND YIELD A GRAY SCALE IMAGE)
-    const float p = 1 /(2.f*PI); // dot between normal and origin ray (ONLY FOR EYE PATH) (THIS ONE IS USED ONLY FOR LAMBERTIAN REFLECTION SINCE OTHERWISE THE BRDF CANCEL EACH OTHER OUT)
+	// Probability distribution function for lambertian reflection BRDF
+	// const vec3 p = glm::abs(glm::dot(normal, -dir)) * BRDF; // dot between normal and origin ray (ONLY FOR EYE PATH) (THIS DOES NOT WORK WITH LAMBERTIAN REFLECTION SINCE THE BRDF EVALUATIONS WILL CANCEL EACH OTHER OUT AND YIELD A GRAY SCALE IMAGE)
+	const float p = 1 /(2.f*PI); // dot between normal and origin ray (ONLY FOR EYE PATH) (THIS ONE IS USED ONLY FOR LAMBERTIAN REFLECTION SINCE OTHERWISE THE BRDF CANCEL EACH OTHER OUT)
 
-    // Recursively trace reflected light sources.
+	// Recursively trace reflected light sources.
 
-    vec3 incoming = TracePath(newstart, newdir, depth + 1);
+	vec3 incoming = TracePath(newstart, newdir, depth + 1);
 
 
-    // Apply the Rendering Equation here. Let ceiling triangle emit light
+	// Apply the Rendering Equation here. Let ceiling triangle emit light
 	vec3 emittance = vec3(triangles[i.triangleIndex]->emission, triangles[i.triangleIndex]->emission, triangles[i.triangleIndex]->emission);
 
 	// light distance thingy is NOT needed because we only regard the light ocming from a point on an emitting surface, whereas the labs calculated some sort of 
 	// projected area onto the point light to deduce how much of the point light shines on the vertex. Further away, there are "less light" in a particular direction.
 
-    //vec3 light = emittance/float(4.0f*PI*glm::length(start - newstart)*glm::length(start - newstart)); // calculate irradiance of light
+	//vec3 light = emittance/float(4.0f*PI*glm::length(start - newstart)*glm::length(start - newstart)); // calculate irradiance of light
 	// emittance = emittance * (glm::dot(surfaceToLight, normal) > 0.0f ? glm::dot(surfaceToLight, normal) : 0.f);
 
-    // assert(vec3(2, 3, 10) / vec3(2, 1, 5) == vec3(1, 3, 2));
+	// assert(vec3(2, 3, 10) / vec3(2, 1, 5) == vec3(1, 3, 2));
 
 	vec3 res =  (BRDF / p * incoming * cos_theta);
 	// if(res.x > 0)
 	// 	cout  << res.x << " " << res.y << " " << res.z << endl;
 
-    return emittance + res;
+	return emittance + res;
 
-  }
+}
 
   //assume startP and endP already assigned
 void FindPath(vec3 start, vec3 direction, int maxDepth, vector<Intersection>& intersections){
@@ -389,10 +362,10 @@ void FindPath(vec3 start, vec3 direction, int maxDepth, vector<Intersection>& in
 	//add next point
 	intersections.push_back(i);
 	//helper function to calculate rest of the points
-	FindPathHelper(i.position, i.normal, direction, 1, maxDepth, intersections);
+	FindPathHelper(i, direction, 1, maxDepth, intersections);
 }
 
-void FindPathHelper(vec3 point, vec3 normalToPoint, vec3 directionToPoint, int currentDepth, int maxDepth, vector<Intersection>& intersections){
+void FindPathHelper(Intersection& point, vec3 directionToPoint, int currentDepth, int maxDepth, vector<Intersection>& intersections){
 	if(currentDepth <= maxDepth){
 		Intersection i;
 		std::uniform_real_distribution<float> dis(0, 1.0);
@@ -400,13 +373,14 @@ void FindPathHelper(vec3 point, vec3 normalToPoint, vec3 directionToPoint, int c
 		float theta1 = acos(1 - 2*dis(rd));
 		vec3 newDirection = glm::normalize(vec3(sin(theta1)*sin(theta0), sin(theta1)*cos(theta0), cos(theta1)));
 		vec3 normalDirection = glm::normalize(-directionToPoint);
+		vec3 normalToPoint = triangles[point.triangleIndex]->normal(point.position);
 		vec3 normal = glm::dot(normalDirection, normalToPoint) * normalToPoint / glm::dot(normalToPoint, normalToPoint);
 		normal = glm::normalize(normal);
 		newDirection = glm::dot(newDirection, normal) * newDirection / glm::dot(newDirection, newDirection);
 		newDirection = glm::normalize(newDirection);
-		if(ClosestIntersection(point, newDirection, triangles, i)){
+		if(ClosestIntersection(point.position, newDirection, triangles, i)){
 			intersections.push_back(i);
-			FindPathHelper(i.position, i.normal, newDirection, currentDepth+1, maxDepth, intersections);
+			FindPathHelper(point, newDirection, currentDepth+1, maxDepth, intersections);
 		}else{
 			//FindPathHelper(point, normalToPoint, directionToPoint, currentDepth, maxDepth, intersections);
 		}
@@ -414,33 +388,65 @@ void FindPathHelper(vec3 point, vec3 normalToPoint, vec3 directionToPoint, int c
 }
 
 vec3 calcRadianceToPoint(const BDPTpath& path, unsigned int i) {
+
 	if(i >= path.intersections.size()){
+		//cout << triangles[path.endP.triangleIndex]->emission << endl;
 		return vec3(0, 0, 0);
 	}else{
-		vec3 p0;
+		Intersection p0;
+		Intersection p1 = path.intersections[i];
+		Intersection p2;
+
 		if(i == 0){
 			p0 = path.startP;
 		}else{
-			p0 = path.intersections[i-1].position;
+			p0 = path.intersections[i-1];
 		}
-		vec3 p1 = path.intersections[i].position;
-		vec3 p2;
+
 		if(i == path.intersections.size() - 1){
 			p2 = path.endP;
 		}else{
-			p2 = path.intersections[i+1].position;
+			p2 = path.intersections[i+1];
 		}
-		vec3 n1 = path.intersections[i].normal;
-		vec3 oldDirection = glm::normalize(p0 - p1);
-		vec3 newDirection = glm::normalize(p2 - p1);
-		vec3 normal = glm::normalize(glm::dot(oldDirection, n1) * n1 / glm::dot(n1, n1));
-		float cos_theta = glm::dot(newDirection, normal);
-		vec3 BRDF = triangles[path.intersections[i].triangleIndex]->color / float(PI);
-		const float p = 1 /(2.f*float(PI));
+
+		// vec3 n1 = triangles[path.intersections[i].triangleIndex]->normal(p1);
+		// vec3 oldDirection = glm::normalize(p0 - p1);
+		// vec3 newDirection = glm::normalize(p2 - p1);
+		// vec3 normal = glm::normalize(glm::dot(oldDirection, n1) * n1 / glm::dot(n1, n1));
+		// float cos_theta = glm::dot(newDirection, normal);
+		// //cout << cos_theta << endl;
+		// vec3 BRDF = triangles[path.intersections[i].triangleIndex]->color / float(PI);
+		// const float p = 1 /(2.f*float(PI));
+		// vec3 incoming = calcRadianceToPoint(path, i+1);
+		// cout << incoming.x << " " << incoming.y << " " << incoming.z << endl;
+		// vec3 emittance = vec3(triangles[path.intersections[i].triangleIndex]->emission, triangles[path.intersections[i].triangleIndex]->emission, triangles[path.intersections[i].triangleIndex]->emission);
+		// //cout << triangles[path.intersections[i].triangleIndex]->emission << endl;
+		// vec3 res = emittance + (BRDF * incoming * cos_theta / p);
+		// cout << res.x << " " << res.y << " " << res.z << endl;
+
+		vec3 newstart = p1.position;
+		vec3 start = p0.position;
+		vec3 newdir = glm::normalize(p2.position - p1.position);
+
+		vec3 surfaceToLight = glm::normalize(start - newstart);
+
+		vec3 normal = glm::dot(surfaceToLight, triangles[p2.triangleIndex]->normal(newstart)) * triangles[p2.triangleIndex]->normal(newstart) / glm::dot(triangles[p2.triangleIndex]->normal(newstart), triangles[p2.triangleIndex]->normal(newstart));
+		
+		normal = glm::normalize(normal); // HAS to be normalized after projection onto another vector.
+		newdir = glm::dot(newdir, normal) * newdir / glm::dot(newdir, newdir);
+		newdir = glm::normalize(newdir);
+
+		float cos_theta = glm::dot(newdir, normal); // note that the cosine factor is part of the Rendering Equation integrand/differential, NOT the BRDF.
+		vec3 BRDF = triangles[p2.triangleIndex]->color / float(PI); // just the reflectance of the material / PI for Lambertian Reflection: http://www.oceanopticsbook.info/view/surfaces/lambertian_brdfs
+
+		const float p = 1 /(2.f*PI); // dot between normal and origin ray (ONLY FOR EYE PATH) (THIS ONE IS USED ONLY FOR LAMBERTIAN REFLECTION SINCE OTHERWISE THE BRDF CANCEL EACH OTHER OUT)
 		vec3 incoming = calcRadianceToPoint(path, i+1);
-		vec3 emittance = vec3(triangles[path.intersections[i].triangleIndex]->emission, triangles[path.intersections[i].triangleIndex]->emission, triangles[path.intersections[i].triangleIndex]->emission);
-		vec3 res = (BRDF / p * incoming * cos_theta);
-		return emittance + res;
+		vec3 emittance = vec3(triangles[p2.triangleIndex]->emission, triangles[p2.triangleIndex]->emission, triangles[p2.triangleIndex]->emission);
+		vec3 res = emittance + (BRDF / p * incoming * cos_theta);
+
+
+		return res;
+		//return p0;
 	}
 
 }
